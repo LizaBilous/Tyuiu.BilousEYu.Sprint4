b886ffef9fd0eaaf4c08fcf2bb3aeaaf127a7f2b
Bilous.liza@bk.ru <Bilous.liza@bk.ru>
2024-11-08 17:27:24 +0500

    Add project files.

---
+++ Tyuiu.BilousEYu.Sprint4.Task0.V2.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.BilousEYu.Sprint4.Task0.V2.Lib;
+namespace Tyuiu.BilousEYu.Sprint4.Task0.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void GetSumOddArrEl()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 5, 3, 7, 1, 3, 9, 8, 7, 9, 4 };
+            int res = ds.GetSumOddArrEl(numsArray);
+            int numsWaitArray = 44;
+            Assert.AreEqual(numsWaitArray, res);
+        }
+    }
+}
